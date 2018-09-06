b772be9b3e83455567bd2fb6cf94533407a03054
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-06 14:42:59 +0200

    Implemented UI Basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Board from './components/Board/Board';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Board />
       </div>
     );
   }
