da2df0045ecaeccfa569dae6be2363d49a7e8738
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-06 15:38:15 +0200

    Added prop-types

--- src/components/CalButton/CalButton.js
+++ src/components/CalButton/CalButton.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import './CalButton.css';
 
 const CalButton = ({ value, id }) => {
   return (
     <div className="button" id={id}>{value}</div>
   )
+}
+
+CalButton.propTypes = {
+  value: PropTypes.string,
+  id: PropTypes.string
 }
 
 export default CalButton;
