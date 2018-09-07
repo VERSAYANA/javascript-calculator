ab7d5f04d7f27a94263d3efb8720d396f1da37bd
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-07 15:14:10 +0200

    Write reducer for newInput and clear

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducer.js
+++ src/reducer.js
@@ -4,27 +4,62 @@ const initialState = {
   input: '',
   result: '',
 };
+const isOperator = (char) => {
+  if (char === '+' || char === '-' || char === 'x' || char === '/') {
+    return true;
+  }
+  return false;
+};
+
+const calResult = (input) => {
+
+}
+
 export default function(state = initialState, action) {
   switch (action.type) {
     case NEW_INPUT:
+      const lastCharIndex = state.input.length - 1;
+      const lastChar = state.input.charAt(lastCharIndex);
+
+      if (isOperator(action.input) && isOperator(lastChar)) {
+        return {
+          input: state.input.slice(0, lastCharIndex) + action.input,
+          result: state.result,
+        };
+      }
+
+      if (action.input === 'AC') {
+        return {
+          input: '',
+          result: 0,
+        };
+      }
+
+      if (action.input === '=') {
+        return {
+          input: state.input,
+          result: calResult(state.input)
+        }
+      }
+
       return {
         input: state.input + action.input,
-        result: state.result
-      }
+        result: state.result,
+      };
       break;
 
     // case CALCULATE_RESULT:
 
     //   break;
 
-    case CLEAR:
-      return {
-        input: '',
-        result: 0
-      }
-      break;
+    // case CLEAR:
+    //   return {
+    //     input: '',
+    //     result: 0,
+    //   };
+    //   break;
 
     default:
-      return state
+      return state;
   }
 }
