1dafa9924197bb3e6cbb3021715cc8d656f773ca
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-10 11:41:35 +0200

    Add switch for changing theme & light theme colors

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { changeLogic } from '../../actions';
+import { changeLogic, changeTheme } from '../../actions';
 import Switch from '../Switch/Switch';
 import './Setting.css';
 
 class Settings extends Component {
   render() {
-    const { changeLogic, logic } = this.props;
+    const { changeLogic, logic, changeTheme, theme } = this.props;
     return (
       <div id="settings">
         <Switch
+          title="Logic"
           change={changeLogic}
           value={logic}
           trueText="Expression"
           falseText="Immediate"
+        />
+        <Switch
+          title="Theme"
+          change={changeTheme}
+          value={theme}
+          trueText="Dark"
+          falseText="Light"
         />
       </div>
     );
@@ -22,9 +30,11 @@ class Settings extends Component {
 
 const mapStateToProps = (state) => ({
   logic: state.logic,
+  theme: state.theme,
 });
 const mapDispatchToProps = (dispatch) => ({
   changeLogic: (logic) => dispatch(changeLogic(logic)),
+  changeTheme: (theme) => dispatch(changeTheme(theme)),
 });
 
 export default connect(
--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -2,21 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Switch.css';
 
-const Switch = ({ change, trueText, falseText, value }) => {
+const Switch = ({ title, change, trueText, falseText, value }) => {
   return (
     <div id="switch">
-      <span>Logic:</span>
+      <span className="switch-title">{title}:</span>
       <div>
         <button
           id="left-button"
-          className={value ? "switch-button switch-active" : "switch-button"}
+          className={value ? 'switch-button switch-active' : 'switch-button'}
           onClick={() => change(true)}
         >
           {trueText}
         </button>
         <button
           id="right-button"
-          className={!value ? "switch-button switch-active" : "switch-button"}
+          className={!value ? 'switch-button switch-active' : 'switch-button'}
           onClick={() => change(false)}
         >
           {falseText}
@@ -27,6 +27,7 @@ const Switch = ({ change, trueText, falseText, value }) => {
 };
 
 Switch.propTypes = {
+  title: PropTypes.string,
   change: PropTypes.func,
   trueText: PropTypes.string,
   falseText: PropTypes.string,
