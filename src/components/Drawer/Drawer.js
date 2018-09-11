5e090f6b93bd6f7f0fe740a1c2191d44c47be9b2
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-11 20:54:50 +0400

    Add more media query to make it look good on small screens (portrait)

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -9,6 +9,7 @@ class Drawer extends Component {
 
     this.setWrapperRef = this.setWrapperRef.bind(this);
     this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.toggleDrawer = this.toggleDrawer.bind(this);
   }
 
   componentDidUpdate() {
@@ -23,9 +24,13 @@ class Drawer extends Component {
     this.wrapperRef = node;
   }
 
+  toggleDrawer() {
+    this.props.toggleDrawer();
+  }
+
   handleClickOutside(event) {
     if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-      this.props.toggleDrawer();
+      this.toggleDrawer();
     }
   }
 
@@ -37,6 +42,14 @@ class Drawer extends Component {
         className={open ? 'drawer-open' : 'drawer-close'}
         ref={this.setWrapperRef}
       >
+        <div id="settings-header">
+          <div id="close-container" onClick={this.toggleDrawer}>
+            <svg viewBox="0 0 24 24">
+              <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+            </svg>
+          </div>
+          <header>Settings</header>
+        </div>
         <Switch
           title="Theme"
           change={changeTheme}
