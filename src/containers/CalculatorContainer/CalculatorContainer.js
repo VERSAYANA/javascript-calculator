d0eeb8f8fb54287d9a470e9e2818d0850d2efcc9
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-16 23:00:06 +0430

    Add space between characters of input

--- src/containers/CalculatorContainer/CalculatorContainer.js
+++ src/containers/CalculatorContainer/CalculatorContainer.js
@@ -3,7 +3,7 @@ import { newInput } from '../../actions';
 import Calculator from '../../components/Calculator/Calculator';
 
 const mapStateToProps = (state) => ({
-  input: state.input,
+  input: state.input.split('').join(' '),
   display: state.display,
 });
 
